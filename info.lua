a86c1b5ec478353d73e21f61782c0f62efd9e11f
Matthew (Gliktch) <github-07IU02@gliktch.com>
2014-03-02 21:07:47 +1000

    Server Information 0.5
    - Smarter handling of update timer
    - Fixed typo in net.connect message
    - Made variable-processing functions local
    - Cleaned up the code a little, removed extraneous comments
    - Added SendHelpText entry for inclusion in /help output

--- info.lua
+++ info.lua
@@ -1,15 +1,14 @@
 PLUGIN.Title = "Server Information"
 PLUGIN.Author = "Gliktch"
-PLUGIN.Version = "0.4"
+PLUGIN.Version = "0.5"
 PLUGIN.Description = "Displays basic information about the server, including hostname, ip:port and common server settings."
 
 function PLUGIN:Init()
   print("Loading Server Information mod...")
   self:AddChatCommand( "info", self.cmdInfo )
   self:CollectValues()
---  if (infotimer) then infotimer:Destroy() end
---  infotimer = timer.Repeat( 15, self.CollectValues )
-  timer.Repeat( 15, self.CollectValues )
+  if (infotimer) then infotimer:Destroy() end
+  infotimer = timer.Repeat( 15, self.CollectValues )
 end
 
 function PLUGIN:CollectValues()
@@ -23,7 +22,6 @@ function PLUGIN:CollectValues()
   sleep    = Rust.sleepers.on
   pvp      = Rust.server.pvp
   craftin  = Rust.crafting.instant
---  craftin  = assert(tostring(Rust.crafting.instant) or function() if Rust.crafting.instant == false then return "false" else return "unset" end end
 
 -- Further useful settings
   maxplay  = Rust.server.maxplayers
@@ -50,29 +48,33 @@ function PLUGIN:CollectValues()
   
 end
 
-function toboolean(var)
+function PLUGIN:toboolean(var)
   return not not var
 end
 
-function round(num, dec)
+function PLUGIN:round(num, dec)
   local pow = 10^(dec or 0)
   return math.floor(num * pow + 0.5) / pow
 end
 
 function PLUGIN:cmdInfo( netuser, args )
 
-local decaytext = ""
-if decaysec == 43200 then
-  decaytext = "at the standard rate."
-elseif decaysec < 43200 then
-  decaytext = "FASTER, about " .. round(43200 / decaysec, 2) .. "x the normal rate."
-elseif decaysec > 43200 then
-  decaytext = "SLOWER, about 1/" .. round(decaysec / 43200, 2) .. "th the normal rate."
-end
+  local decaytext = ""
+  if decaysec == 43200 then
+      decaytext = "at the standard rate."
+  elseif decaysec < 43200 then
+      decaytext = "FASTER, about " .. round(43200 / decaysec, 2) .. "x the normal rate."
+  elseif decaysec > 43200 then
+      decaytext = "SLOWER, about 1/" .. round(decaysec / 43200, 2) .. "th the normal rate."
+  end
 
   rust.SendChatToUser( netuser, "Server: " .. hostname .. " (" .. tostring( #rust.GetAllNetUsers() ) .. "/" .. maxplay .. ")")
-  rust.SendChatToUser( netuser, "To connect manually, you can use use net.connect " .. hostip .. ":" .. hostport .. " in the F1 console.")
+  rust.SendChatToUser( netuser, "To connect manually, you can use net.connect " .. hostip .. ":" .. hostport .. " in the F1 console.")
   rust.SendChatToUser( netuser, "Sleepers are " .. (toboolean(sleep) and "ON" or "OFF") .. ", PVP is " .. (toboolean(pvp) and "ON" or "OFF") .. ", Fall Damage is " .. (toboolean(falldmg) and "ON" or "OFF") .. ", and Decay is " .. decaytext)
   rust.SendChatToUser( netuser, "Instant crafting is " .. (toboolean(craftin) and "ON" or "OFF, but the Crafting Timescale is " .. craftts .. " and reduced by " .. craftws .. "x when at a Workbench."))
 
+end
+
+function PLUGIN:SendHelpText( netuser )
+	rust.SendChatToUser( netuser, "Use /info to list the details and basic settings of this server." )
 end
